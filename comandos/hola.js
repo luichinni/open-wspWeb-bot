b25c643683e0bf98700709a8f776885007791ccb
Luciano <98102676+luichinni@users.noreply.github.com>
2023-07-10 01:55:24 -0300

    Delete hola.js

--- comandos/hola.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    comando: 'hola', // nombre del comando
-    visible: true, // visible al usuario
-    info: '-> responde hola a quien use el comando' // informacion del comando
-}
-module.exports.ejecutar = function (msg) { // funcion del comando, siempre recibe msg
-    msg.reply('Holiwis!!');
-}
