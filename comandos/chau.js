48c9520027cc2b47dbb9d82169e9c53f6a650997
Luciano <98102676+luichinni@users.noreply.github.com>
2023-07-10 01:55:04 -0300

    Delete chau.js

--- comandos/chau.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    comando : 'chau', // nombre del comando
-    params: '', // parametros del comando
-    visible: true, // visible al usuario
-    info: '-> responde chau a quien use el comando' // informacion del comando
-}
-module.exports.ejecutar = function (msg) { // funcion del comando, siempre reciben msg
-    msg.reply('Chaucha!!');
-}
